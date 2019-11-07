29b5739c1ffe803dcf4b96c3959629790e485b74
amartinez653 <amartinez653@toromail.csudh.edu>
2019-11-06 16:38:18 -0800

    first c file

---
+++ testc.c
@@ -0,0 +1,12 @@
+/*Andrew Martinez
+ * CSC 251
+ * GithubTest*/
+#include <stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	int x=0;
+	printf("Enter a number between 1 and 10: ");
+	scanf("%d",&x);
+	printf("Your number was %d\n",x);
+}
